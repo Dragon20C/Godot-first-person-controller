634840be274a4d2dfbf7497f318eb410a828e7f9
Dragon20C <Dragon212c@gmail.com>
2023-10-23 16:35:36 +0100

    Added footsteps.

--- Data/Scripts/Motion.gd
+++ Data/Scripts/Motion.gd
@@ -15,6 +15,7 @@ extends Node
 @export var feet_audio_player : AudioStreamPlayer3D
 
 @export_group("Footstep")
+var stepped : bool = false
 @export var footstep_sounds : Array[AudioStreamWAV]
 
 var time : float = 0.0
@@ -22,7 +23,7 @@ var time : float = 0.0
 func _physics_process(delta):
 		
 	if enable_headbob:
-		if Player.velocity.length() > 0:
+		if Player.velocity.length() > 0 and Player.is_on_floor():
 			time += delta * Player.velocity.length() * float(Player.is_on_floor())
 			Camera.transform.origin = Motion()
 			if enable_stab:
@@ -35,12 +36,22 @@ func Motion() -> Vector3:
 	var Pos = Vector3.ZERO
 	Pos.y += sin(time * Frequency) * Amplitude
 	Pos.x += cos(time * Frequency / 2) * Amplitude * 2
+	
+	CanPlayFootStep(Pos.y)
+	
 	return Pos
 	
 func RestartPosition(delta):
 	if Camera.transform.origin == Vector3.ZERO: return
 	Camera.transform.origin = Camera.transform.origin.lerp(Vector3.ZERO,10 * delta)
 	time = 0.0
+
+func CanPlayFootStep(y_value : float):
+	if y_value < 0 and !stepped:
+		PlayFootStepSound()
+		stepped = true
+	elif y_value >0 and stepped:
+		stepped = false
 
 func PlayFootStepSound():
 	var random_index : int = randi_range(0,footstep_sounds.size() - 1)
